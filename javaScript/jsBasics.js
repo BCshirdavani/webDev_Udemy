607428914488256910cd0686da8ca1575e244d02
shiMac <beau.shirdavani@bellevuecollege.edu>
2018-12-14 11:49:21 -0800

    include js file in html

---
+++ javaScript/jsBasics.js
@@ -0,0 +1,11 @@
+// javascript basics
+
+clear()
+
+// alert
+alert("random alert");
+// console log
+console.log("log this stuff");
+// prompt - get input from user
+prompt("what is your name?: ");
+var userName = prompt("enter your name:");
