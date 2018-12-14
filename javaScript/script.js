28e9cd47bb73ddf5ba2f1eed76ecbb284b1444e3
shiMac <beau.shirdavani@bellevuecollege.edu>
2018-12-14 15:00:44 -0800

    intro / basic js

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,3 +1,22 @@
 var userName = prompt("what is your name?:");
 alert("nice to meet you " + userName);
 console.log("Name is " + userName);
+
+// while loop
+var count = 1;
+while (count <= 6){
+    console.log("count is : " + count);
+    count++;
+}
+//  for loop
+for(var counter = 0; counter < 6; counter++){
+    console.log("counter is: " + counter);
+}
+
+// functions
+function sayHiTo(name){
+    console.log("hello " + name);
+    console.log("goodbye " + name);
+}
+sayHiTo(userName);
+
