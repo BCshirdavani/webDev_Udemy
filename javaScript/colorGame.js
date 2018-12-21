7e1ffad3ed49757d9feab00bf9438f9b1225346b
shiMac <beau.shirdavani@bellevuecollege.edu>
2018-12-21 15:16:39 -0800

    colors randomly generate

--- javaScript/colorGame.js
+++ javaScript/colorGame.js
@@ -1,12 +1,6 @@
 console.log("connected");
 
-var colors = ["rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(0, 255, 255)",];
-// var pickedColor = colors[3];
+var colors = generateRandomColors(6);
 var pickedColor = pickColor();
 
 var messageDisplay = document.querySelector("#message");
@@ -52,4 +46,22 @@ function changeColors(color){
 function pickColor(){
     var random = Math.floor(Math.random() * (colors.length));
     return colors[random];
+}
+
+
+function generateRandomColors(num){
+    var arr = [];
+    for (var i = 0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var color = "rgb(" + r + ", " + g + ", " + b + ")";
+    return color;
 }
